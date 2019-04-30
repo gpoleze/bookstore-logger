dac533db5212789b6215b8b624695ba0d3fd9836
Gabriel Poleze Ferreira <gabriel.poleze@gmail.com>
2019-04-29 23:37:13 -0700

    moved the routing to outside the App component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Home from "./components/Home/home";
+import AuthorBox from "./components/author-box/author-box";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((
+    <BrowserRouter>
+        <App>
+            <Switch>
+                <Route exact path="/" component={Home}/>
+                <Route path="/author" component={AuthorBox}/>
+                <Route path="/book"/>
+            </Switch>
+        </App>
+    </BrowserRouter>
+), document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
