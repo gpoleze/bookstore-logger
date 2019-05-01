12b3cb1adc1f2bc24ac4ceaa216b71ffc577e19a
Gabriel Poleze Ferreira <gabriel.poleze@gmail.com>
2019-05-01 00:08:35 -0700

    abstracted the API access to set of functions

---
+++ src/components/core/api-handler.js
@@ -0,0 +1,25 @@
+import {ErrorHandler} from "../common/error-handler";
+
+const API = 'http://localhost:8080/api';
+
+export function apiGet(service) {
+    return fetch(API + service, {method: 'GET'})
+        .then(response => response.json())
+        .catch(err => console.log(err));
+}
+
+export function apiPost(service, data) {
+    console.log(data);
+
+
+    return fetch(API + service,
+        {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json',},
+            body: JSON.stringify(data),
+            credentials: "same-origin"
+        })
+        .then(response => response.json())
+        .then(ErrorHandler.handleError)
+        .catch(err => console.log('Error while attempting to post:', err));
+}
