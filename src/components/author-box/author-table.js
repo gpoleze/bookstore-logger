4cf31be8131de86bb6b0d5222bb40bd2752b8516
Gabriel Poleze Ferreira <gabriel.poleze@gmail.com>
2019-05-04 22:57:58 -0700

    implemented Material Ui Tables

--- src/components/author-box/author-table.js
+++ src/components/author-box/author-table.js
@@ -1,29 +1,30 @@
 import React, {Component} from 'react';
+import {Paper, Table, TableHead, TableBody, TableRow, TableCell} from "@material-ui/core";
 
 export default class AuthorTable extends Component {
     render() {
         return (
-            <div>
-                <table className="pure-table">
-                    <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Email</th>
-                    </tr>
-                    </thead>
-                    <tbody>
+            <Paper>
+                <Table>
+                    <TableHead>
+                    <TableRow>
+                        <TableCell>Name</TableCell>
+                        <TableCell>Email</TableCell>
+                    </TableRow>
+                    </TableHead>
+                    <TableBody>
                     {
                         this.props.authors.map(author =>
                             (
-                                <tr key={author.id}>
-                                    <td>{author.nome}</td>
-                                    <td>{author.email}</td>
-                                </tr>
+                                <TableRow key={author.id}>
+                                    <TableCell>{author.nome}</TableCell>
+                                    <TableCell>{author.email}</TableCell>
+                                </TableRow>
                             ))
                     }
-                    </tbody>
-                </table>
-            </div>
+                    </TableBody>
+                </Table>
+            </Paper>
         );
     }
 
--- src/components/book-box/book-table.js
+++ src/components/book-box/book-table.js
@@ -1,29 +1,32 @@
 import React from "react";
+import {Paper, Table, TableHead, TableBody, TableRow, TableCell} from "@material-ui/core";
 
 export default class BookTable extends React.Component {
     render() {
         return (
-            <div>
-                <table className="pure-table">
-                    <thead>
-                    <tr>
-                        <th>Title</th>
-                        <th>Price</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {
-                        this.props.books.map(book =>
-                            (
-                                <tr key={book.id}>
-                                    <td>{book.titulo}</td>
-                                    <td>{book.preco}</td>
-                                </tr>
-                            ))
-                    }
-                    </tbody>
-                </table>
-            </div>
+            <Paper>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Title</TableCell>
+                            <TableCell>Price</TableCell>
+                        </TableRow>
+                    </TableHead>
+
+                    <TableBody>
+                        {
+                            this.props.books.map(book =>
+                               (
+                                    <TableRow key={book.id}>
+                                        <TableCell>{book.titulo}</TableCell>
+                                        <TableCell>{book.preco}</TableCell>
+                                    </TableRow>
+                                )
+                            )
+                        }
+                    </TableBody>
+                </Table>
+            </Paper>
         );
     }
 }
